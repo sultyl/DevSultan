407081a6b6b9f47fab148e9321e45094abd77522
sultyl <70979325+sultyl@users.noreply.github.com>
2023-03-17 10:13:18 -0700

    Renaming my resume

---
+++ build/precache-manifest.53652ea7c61220209f5d3b24ccc29290.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7f9c1c64524ba699d18",
+    "url": "/DevSultan/static/css/main.e4f4bf65.chunk.css"
+  },
+  {
+    "revision": "d7f9c1c64524ba699d18",
+    "url": "/DevSultan/static/js/main.d7f9c1c6.chunk.js"
+  },
+  {
+    "revision": "01d68f57e654ac38a9a2",
+    "url": "/DevSultan/static/js/1.01d68f57.chunk.js"
+  },
+  {
+    "revision": "0150e6ea115801ca381c",
+    "url": "/DevSultan/static/js/runtime~main.0150e6ea.js"
+  },
+  {
+    "revision": "48ef1e8cf26d1e71e9f66127584f042c",
+    "url": "/DevSultan/static/media/Ola.48ef1e8c.png"
+  },
+  {
+    "revision": "03f8a775fcad9be0062a1e52a29b00f4",
+    "url": "/DevSultan/static/media/Bus.03f8a775.jpg"
+  },
+  {
+    "revision": "2d8356d7fcb578850b9ef0fd69d2b175",
+    "url": "/DevSultan/static/media/fit.2d8356d7.jpg"
+  },
+  {
+    "revision": "698c63ccdb9e5768a5d5b99824c73551",
+    "url": "/DevSultan/static/media/Por.698c63cc.jpg"
+  },
+  {
+    "revision": "6fe3d6997d02b5ab35bb5444abd09908",
+    "url": "/DevSultan/static/media/Res.6fe3d699.jpg"
+  },
+  {
+    "revision": "02d1539d1633609e38794a6414b5b3a0",
+    "url": "/DevSultan/static/media/Job.02d1539d.jpg"
+  },
+  {
+    "revision": "ae4d696ef345abcada994f48f5324870",
+    "url": "/DevSultan/index.html"
+  }
+];
