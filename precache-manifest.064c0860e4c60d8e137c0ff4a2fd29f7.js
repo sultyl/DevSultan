7a275d695b56ff644e825960ee0800489c555146
sultyl <70979325+sultyl@users.noreply.github.com>
2023-08-23 04:49:14 +0100

    Updates

---
+++ precache-manifest.064c0860e4c60d8e137c0ff4a2fd29f7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "286620b936fbf1043d59",
+    "url": "/DevSultan/static/css/main.2199528a.chunk.css"
+  },
+  {
+    "revision": "286620b936fbf1043d59",
+    "url": "/DevSultan/static/js/main.286620b9.chunk.js"
+  },
+  {
+    "revision": "cfd49c37edebb853d76a",
+    "url": "/DevSultan/static/js/1.cfd49c37.chunk.js"
+  },
+  {
+    "revision": "0150e6ea115801ca381c",
+    "url": "/DevSultan/static/js/runtime~main.0150e6ea.js"
+  },
+  {
+    "revision": "48ef1e8cf26d1e71e9f66127584f042c",
+    "url": "/DevSultan/static/media/Ola.48ef1e8c.png"
+  },
+  {
+    "revision": "03f8a775fcad9be0062a1e52a29b00f4",
+    "url": "/DevSultan/static/media/Bus.03f8a775.jpg"
+  },
+  {
+    "revision": "2d8356d7fcb578850b9ef0fd69d2b175",
+    "url": "/DevSultan/static/media/fit.2d8356d7.jpg"
+  },
+  {
+    "revision": "698c63ccdb9e5768a5d5b99824c73551",
+    "url": "/DevSultan/static/media/Por.698c63cc.jpg"
+  },
+  {
+    "revision": "6fe3d6997d02b5ab35bb5444abd09908",
+    "url": "/DevSultan/static/media/Res.6fe3d699.jpg"
+  },
+  {
+    "revision": "02d1539d1633609e38794a6414b5b3a0",
+    "url": "/DevSultan/static/media/Job.02d1539d.jpg"
+  },
+  {
+    "revision": "b7cfe94f6500863e774de89ecddf8d24",
+    "url": "/DevSultan/static/media/odogwu.b7cfe94f.png"
+  },
+  {
+    "revision": "2291ae008ade595643280ef72f45a583",
+    "url": "/DevSultan/static/media/codearcade.2291ae00.jpeg"
+  },
+  {
+    "revision": "b367d690142ffe57473bf3a597985374",
+    "url": "/DevSultan/index.html"
+  }
+];
