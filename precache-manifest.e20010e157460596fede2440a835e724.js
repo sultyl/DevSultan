030a3d180cc4f7321c28ea986fd2b40b43d04254
sultyl <70979325+sultyl@users.noreply.github.com>
2023-08-23 03:54:40 +0100

    updated

--- precache-manifest.e20010e157460596fede2440a835e724.js
+++ precache-manifest.e20010e157460596fede2440a835e724.js
@@ -1,46 +1,54 @@
 self.__precacheManifest = [
   {
-    "revision": "f001419bb8ed15cd6ca1",
-    "url": "/myportfolio/static/css/main.26176d4e.chunk.css"
+    "revision": "286620b936fbf1043d59",
+    "url": "/build/static/css/main.2199528a.chunk.css"
   },
   {
-    "revision": "f001419bb8ed15cd6ca1",
-    "url": "/myportfolio/static/js/main.f001419b.chunk.js"
+    "revision": "286620b936fbf1043d59",
+    "url": "/build/static/js/main.286620b9.chunk.js"
   },
   {
-    "revision": "94d0ac11772740e39c35",
-    "url": "/myportfolio/static/js/1.94d0ac11.chunk.js"
+    "revision": "cfd49c37edebb853d76a",
+    "url": "/build/static/js/1.cfd49c37.chunk.js"
   },
   {
-    "revision": "6b6f225bb9dac2e07552",
-    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+    "revision": "0150e6ea115801ca381c",
+    "url": "/build/static/js/runtime~main.0150e6ea.js"
   },
   {
     "revision": "48ef1e8cf26d1e71e9f66127584f042c",
-    "url": "/myportfolio/static/media/Ola.48ef1e8c.png"
+    "url": "/build/static/media/Ola.48ef1e8c.png"
   },
   {
     "revision": "03f8a775fcad9be0062a1e52a29b00f4",
-    "url": "/myportfolio/static/media/Bus.03f8a775.jpg"
+    "url": "/build/static/media/Bus.03f8a775.jpg"
   },
   {
     "revision": "2d8356d7fcb578850b9ef0fd69d2b175",
-    "url": "/myportfolio/static/media/fit.2d8356d7.jpg"
+    "url": "/build/static/media/fit.2d8356d7.jpg"
   },
   {
     "revision": "698c63ccdb9e5768a5d5b99824c73551",
-    "url": "/myportfolio/static/media/Por.698c63cc.jpg"
+    "url": "/build/static/media/Por.698c63cc.jpg"
   },
   {
     "revision": "6fe3d6997d02b5ab35bb5444abd09908",
-    "url": "/myportfolio/static/media/Res.6fe3d699.jpg"
+    "url": "/build/static/media/Res.6fe3d699.jpg"
   },
   {
     "revision": "02d1539d1633609e38794a6414b5b3a0",
-    "url": "/myportfolio/static/media/Job.02d1539d.jpg"
+    "url": "/build/static/media/Job.02d1539d.jpg"
   },
   {
-    "revision": "eb844765b84060fe1b5c0b0ffb49b224",
-    "url": "/myportfolio/index.html"
+    "revision": "b7cfe94f6500863e774de89ecddf8d24",
+    "url": "/build/static/media/odogwu.b7cfe94f.png"
+  },
+  {
+    "revision": "2291ae008ade595643280ef72f45a583",
+    "url": "/build/static/media/codearcade.2291ae00.jpeg"
+  },
+  {
+    "revision": "c5de857192c923bff2fe60be7cc75a23",
+    "url": "/build/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myportfolio/precache-manifest.e20010e157460596fede2440a835e724.js"
+  "/build/precache-manifest.bd8568dfd0770c0524279f8d92631e02.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myportfolio/index.html", {
+workbox.routing.registerNavigationRoute("/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
